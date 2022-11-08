d808aecc63f0afbdabec205bbf4dca954d5e8f52
Maycol Dotel <maycoldotel@gmail.com>
2022-11-08 11:31:12 -0400

    Cambiar html por php

---
+++ ClaseImg/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../ClaseListas/Css/style.css">
+
+</head>
+<body>
+    <header>
+        <nav>
+            <figure>
+                <img src="./Img/descarga.png" alt="logo">
+            </figure>
+                
+            </div>
+            <div class="nav-Secciones">
+                <ul>
+                    <li>Inicio</li>
+                    <li>Nosotros</li>
+                    <li>Contactos</li>
+                    <li>Servicios</li>
+                </ul>
+            </div>
+                
+        </nav>
+    </header>
+    <main>
+        <section>
+            <table BORDER style="margin: 0 auto; margin-top: 250px;">
+                
+                <tr>
+                    <th><b>Nombre</b></th>
+                    <th><b>Apellido</b></th>
+                    <th><b>Curso</b></th>
+                </tr>
+                <tr>
+                    <td>Maycol</td>
+                    <td>Dotel</td>
+                    <td>6to Info</td>
+                </tr>
+            </table>
+            
+        </section>
+    </main>
+</body>
+</html>
